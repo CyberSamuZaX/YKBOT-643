5843c20ea2634ad6cf189b6e141135c442e52bc6
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-01-13 07:19:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "zzQGQDiT#5ZKiu5XEjgFhv5RurJBR8PC5w-QbvfY2L9hGpP29yG4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/Fu9lzTw.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am Youssouf I am alive now",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
